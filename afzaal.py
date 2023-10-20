5bd239cbb4359d8a4f552df6b1825638b93d9a92
mehar afzaal <140016461+code-cracker-pro@users.noreply.github.com>
2023-10-20 23:49:30 +0500

    Create afzaal.py

---
+++ afzaal.py
@@ -0,0 +1,54 @@
+import sys
+import asyncio
+import aiohttp
+
+async def enumerate_subdomains(url, wordlist, output_format="txt"):
+    live_subdomains = []
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+
+        async def check_subdomain(subdomain):
+            full_url = f"http://{subdomain}.{url}"
+            try:
+                async with session.get(full_url, timeout=5) as response:
+                    if response.status == 200:
+                        status = "L"
+                        live_subdomains.append(f"{full_url} ({status})")
+                    else:
+                        status = "X"
+                    if output_format == "json":
+                        print(f'{{"url": "{full_url}", "status": "{status}"}}')
+                    else:
+                        print(f"{full_url} ({status})")
+            except aiohttp.ClientError:
+                pass
+
+        for subdomain in wordlist:
+            task = asyncio.ensure_future(check_subdomain(subdomain))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+    if output_format != "json":
+        print("\nLive Subdomains:")
+        for subdomain in live_subdomains:
+            print(subdomain)
+
+def main():
+    if len(sys.argv) != 3 or sys.argv[1] != "-u":
+        print("Usage: afzaal.py -u http://example.com")
+        sys.exit(1)
+
+    url = sys.argv[2]
+
+    with open('custom_wordlist.txt', 'r') as file:
+        wordlist = [line.strip() for line in file]
+
+    print(f"Checking subdomains for {url}:\n")
+
+    output_format = "txt"  # Change to "json" to output results in JSON format
+    asyncio.run(enumerate_subdomains(url, wordlist, output_format))
+
+if _name_ == "_main_":
+    main()
